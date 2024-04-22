06f11549dcaa227eca5a6e09c7f8ddbbfd6a45b5
Merlyin <focastefan13@gmail.com>
2024-04-22 14:25:55 +0300

    okay
    let's see now

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+const saveCurrentTime = throttle(() => {
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}, 1000);
+
+player.on('timeupdate', saveCurrentTime);
+
+if (localStorage.getItem('videoplayer-current-time') !== null) {
+  const currentTime = parseFloat(
+    localStorage.getItem('videoplayer-current-time')
+  );
+  player.setCurrentTime(currentTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const feedbackFormVal = document.querySelector('.feedback-form');
+const emailInput = feedbackFormVal.querySelector('input[name="email"]');
+const msgInput = feedbackFormVal.querySelector('textarea[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+if (savedData) {
+  emailInput.value = savedData.email;
+  msgInput.value = savedData.message;
+}
+
+const saveForm = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: msgInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
+}, 500);
+
+feedbackFormVal.addEventListener('input', saveForm);
+
+feedbackFormVal.addEventListener('submit', event => {
+  event.preventDefault();
+  const state = {
+    email: emailInput.value,
+    message: msgInput.value,
+  };
+  console.log(state);
+  localStorage.removeItem(STORAGE_KEY);
+  emailInput.value = '';
+  msgInput.value = '';
+});
